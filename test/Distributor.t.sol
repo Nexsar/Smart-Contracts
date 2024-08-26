3a752228c631b47ff50ad91ad5c68c2d9646a83d
Abhinav Prakash <abhinav.prakash319@gmail.com>
2024-08-26 23:35:09 +0530

    fix::> Basic test setup for Distributor

--- test/Distributor.t.sol
+++ test/Distributor.t.sol
@@ -6,4 +6,13 @@ import {Distributors} from "../src/Distributor.sol";
 
 contract CounterTest is Test {
     Distributors public distributor;
+
+    address public distributor1 = makeAddr("distributor1");
+    address public distributor2 = makeAddr("distributor2");
+
+    function setUp() public {
+        distributor = new Distributors();
+        vm.deal(distributor1, 10 ether);
+        vm.deal(distributor2, 5 ether);
+    }
 }
